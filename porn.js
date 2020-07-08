0f0f955f6aa1861562563dfaeba5b5c44111c9c7
Imagonow55 <55607616+Imagonow55@users.noreply.github.com>
2020-07-07 21:44:44 -0400

    Web files

---
+++ porn.js
@@ -0,0 +1,9 @@
+var password;
+var passw = ("test");
+password=prompt("Please enter the daily password.");
+if (password == passw) {
+    alert("The password is correct. Pres ok to view the webpage");
+}
+    else {
+        window.location = ("failed.html");
+};
